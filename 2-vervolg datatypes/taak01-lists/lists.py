f1cc68b519132392d4ec403a5cec68f934e7e82c
BruceAmende <bruceamende32@gmail.com>
2020-10-26 15:52:53 +0100

    taak 2/01 af

---
+++ 2-vervolg datatypes/taak01-lists/lists.py
@@ -0,0 +1,12 @@
+
+
+maanden = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december']
+
+
+
+
+for i in range(5):
+    userInput = input( "Geef een geheel getal : ")
+    userInput = int( userInput )
+    maanden.append(userInput)
+    print(maanden)
