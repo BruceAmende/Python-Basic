b484eee0fb20b332c2d58d4c347ac89c6aa5e8a1
BruceAmende <bruceamende32@gmail.com>
2020-10-26 15:59:10 +0100

    taak 2/02 af

---
+++ 2-vervolg datatypes/taak02 -dicts/dicts.py
@@ -0,0 +1,10 @@
+Provinciehoofdsteden = dict(
+Noord_Holland='Amsterdam',
+Zuid_Holland='Den_Haag',
+Utrecht='Utrecht',
+)
+
+type(Provinciehoofdsteden)
+
+Provinciehoofdsteden['Zeeland'] = 'Middelburg'
+dict.values(Provinciehoofdsteden)
